8c7cfa520e254126eb8b9af4cb55efa9d53508c6
[bisha21] <[timilsinab22bi@gmail.com]>
2025-06-25 21:03:13 +0545

    setup express and typescript

---
+++ server.ts
@@ -0,0 +1,14 @@
+import app from "./src/app";
+import { envConfig } from "./src/config/config";
+
+
+
+function startServer()
+{
+    const port = envConfig.port|| 5000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+startServer();
